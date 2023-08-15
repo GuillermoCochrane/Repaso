12bc4261aefaabc6c4e24382de3f1c3e289e7f41
GuillermoCochrane <guilleac81@gmail.com>
2023-08-15 19:07:37 -0300

    Finalizado inciso 7 del ejercico de cierre de modulo

---
+++ modulo 2 - Introducción a Node.js y JavaScript/Clase 7- Cierre del módulo/concesionaria.js
@@ -0,0 +1,17 @@
+let autos = require('./autos');
+let concesionaria ={
+
+    autos: autos,
+
+    buscar: function(){
+
+    },
+
+    vender: function(){
+
+    }
+
+}
+
+
+console.log(concesionaria);
