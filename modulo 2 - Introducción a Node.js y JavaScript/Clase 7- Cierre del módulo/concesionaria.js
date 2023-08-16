e545250fec805fb8f35826140976726b5a7395f5
GuillermoCochrane <guilleac81@gmail.com>
2023-08-16 19:13:47 -0300

    Finalizado inciso 14

--- modulo 2 - Introducción a Node.js y JavaScript/Clase 7- Cierre del módulo/concesionaria.js
+++ modulo 2 - Introducción a Node.js y JavaScript/Clase 7- Cierre del módulo/concesionaria.js
@@ -2,7 +2,7 @@ let autos = require('./autos');
 let concesionaria ={
 
     autos: autos,
-
+    // busca un auto ingresando como parametro la patente
     buscarAuto: function(patente){
         let resultadoBusqueda = this.autos.filter(auto => auto.patente == patente);
         if (resultadoBusqueda == false){            
@@ -11,6 +11,7 @@ let concesionaria ={
         return resultadoBusqueda[0]
     },
 
+    // cambia el estado de un auto sin vender (false) a vendido (true)
     venderAuto : function(patente){
         let autoAvender = this.buscarAuto(patente)
         if (autoAvender != null){
@@ -19,6 +20,7 @@ let concesionaria ={
         return autoAvender
     },
 
+    // develve un array de objetos con los autos disponible para la venta
     autosParaLaVenta: function(){
         let autosDisponibles = this.autos.filter(auto => auto.vendido == false)
         if (autosDisponibles == false){            
@@ -27,20 +29,37 @@ let concesionaria ={
         return autosDisponibles
     },
 
+    //auto 0 km es aquel que tenga un kilometraje menor a 100.
     autosNuevos: function(){
         return this.autosParaLaVenta().filter( autoNuevo =>autoNuevo.km < 100 );
     },
 
+    //devuelve una lista que contiene el precio de venta de cada auto vendido.
     listaDeVentas: function(){
         let autosVendidos = this.autos.filter(auto => auto.vendido == true);
         return autosVendidos.map(auto => auto.precio);
     },
 
+    //Nos devuelva la sumatoria del valor de todas las ventas realizadas.
     totalDeVentas: function(){
-        return this.listaDeVentas().reduce((acumulador, precio) =>  acumulador+precio);
+        if(this.listaDeVentas().length == 0){
+            return 0
+        }else{
+            return this.listaDeVentas().reduce((acumulador, precio) =>  acumulador+precio);
+        };
+    },
+
+    puedeComprar: function(auto,persona){
+        let autoPorComprar = this.buscarAuto(auto)
+        return ((autoPorComprar.precio/autoPorComprar.cuotas) <= persona.capacidadDePagoEnCuotas && autoPorComprar.precio <= persona.capacidadDePagoTotal);
     }
 
 }
 
+let persona = {
+    nombre: 'Juan',
+    capacidadDePagoEnCuotas: 20000,
+    capacidadDePagoTotal: 100000
+};
 
-console.log(concesionaria.totalDeVentas());
+console.log(concesionaria.puedeComprar("NBC911", persona));
