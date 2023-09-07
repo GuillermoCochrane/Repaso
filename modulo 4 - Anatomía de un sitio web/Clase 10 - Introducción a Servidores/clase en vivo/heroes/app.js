b8708e33251fc6b61ccdf237d308181086514b2e
GuillermoCochrane <guilleac81@gmail.com>
2023-09-06 21:52:26 -0300

    Optimizado el codigo de la app

--- modulo 4 - Anatomía de un sitio web/Clase 10 - Introducción a Servidores/clase en vivo/heroes/app.js
+++ modulo 4 - Anatomía de un sitio web/Clase 10 - Introducción a Servidores/clase en vivo/heroes/app.js
@@ -1,9 +1,21 @@
+// Dependencias,  datos y configuraciones
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3030;
 
 app.use(express.static("./public"));
+
+// funcion para generear rutas
+let routerHTML = (endpoint) =>{
+    app.get('/'+endpoint, (req,res) => { 
+        res.sendFile(path.join( __dirname ,"./views/"+endpoint+".html"))
+    } );
+};
+
+let endpoints = ["babbage","berners-lee","hamilton","hopper","lovelace","turing","clarke"];
+
+//sistema de ruteo
 
 app.get('/', (req,res) => { 
     res.sendFile(path.join( __dirname ,"./views/index.html"))
@@ -13,6 +25,9 @@ app.get('/home', (req,res) => {
     res.sendFile(path.join( __dirname ,"./views/index.html"))
 } );
 
+endpoints.forEach((endpoint)=> routerHTML(endpoint));
+
+/* 
 app.get('/babbage', (req,res) => { 
     res.sendFile(path.join( __dirname ,"./views/babbage.html"))
 } );
@@ -40,8 +55,9 @@ app.get('/turing', (req,res) => {
 app.get('/clarke', (req,res) => { 
     res.sendFile(path.join( __dirname ,"./views/clarke.html"))
 } );
+ */
 
-
+//ponemos el servidor en marcha
 
 app.listen(port, ()=>{console.log("Levantando servidor en puerto " + port + ": http://localhost:" + port)
 });
