30002335e5bcc2fa79e04e8521dbafc24fdf0a7c
GuillermoCochrane <guilleac81@gmail.com>
2023-09-11 18:55:53 -0300

    Finalizado ejercicio del inciso  'Elementos de línea y de bloque' de la Clase 11

--- modulo 4 - Anatomía de un sitio web/Clase 11 - Creando la estructura de un sitio web/4 - Elementos de línea y de bloque/app.js
+++ modulo 4 - Anatomía de un sitio web/Clase 11 - Creando la estructura de un sitio web/4 - Elementos de línea y de bloque/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname,"./views/index.html"))
