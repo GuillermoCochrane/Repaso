cf53eb296f679947227def9f755cf16784be3d6f
GuillermoCochrane <guilleac81@gmail.com>
2023-09-18 15:11:55 -0300

    Finalizado inciso 2: 'Formularios' de la Clase 15: 'Trabajando con Formularios'

---
+++ modulo 4 - Anatomía de un sitio web/Clase 15 - Trabajando con Formularios/2 - Formularios/app.js
@@ -0,0 +1,30 @@
+//modulos , configuraciones, datos
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+const publicPath = path.resolve(__dirname , './public');
+
+//seteamos la carpeta de recursos estáticos
+app.use(express.static(publicPath));
+
+// funcion para generear rutas
+let routerHTML = (endpoint) =>{
+    app.get('/'+endpoint, (req,res) => { 
+        res.sendFile(path.join( __dirname ,"./views/"+endpoint+".html"))
+    } );
+};
+let endpoints = ["primer-formulario","campos-de-texto","mas-campos-de-texto","completar-el-formulario"]
+
+//ruteo
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/index.html"));
+})
+
+endpoints.forEach((endpoint)=> routerHTML(endpoint));
+
+//Levantamos el servidor
+app.listen(port, ()=>{console.log("Levantando servidor en puerto " + port + ": http://localhost:" + port)
+});
