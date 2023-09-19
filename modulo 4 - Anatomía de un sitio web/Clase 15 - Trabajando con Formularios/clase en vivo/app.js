e89276541554a57d6e89c1542e79e260e8540d7d
GuillermoCochrane <guilleac81@gmail.com>
2023-09-19 00:07:17 -0300

    Finalizado Micro desafío - Paso 2 de la clase 15

--- modulo 4 - Anatomía de un sitio web/Clase 15 - Trabajando con Formularios/clase en vivo/app.js
+++ modulo 4 - Anatomía de un sitio web/Clase 15 - Trabajando con Formularios/clase en vivo/app.js
@@ -15,15 +15,25 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,"./views/home.html"))
 });
 
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname,"./views/login.html"))
+});
+
 app.get('/register', (req,res) => {
     res.sendFile(path.join(__dirname,"./views/register.html"))
+});
+
+app.post('/', (req,res) => {
+    res.sendFile(path.join(__dirname,"./views/home.html"))
 });
 
 app.post('/register', (req,res) => {
     res.sendFile(path.join(__dirname,"./views/home.html"))
 });
 
-//levantamos el server
+
+
+//levantamos el server 
 
 app.listen(port, ()=>{console.log("Levantando servidor en puerto " + port + ": http://localhost:" + port)
 });
