f73f1e19b24c8f31fbce93e5bdcd107327857a59
GuillermoCochrane <guilleac81@gmail.com>
2024-03-29 19:22:44 -0300

    finalizado Bonus Track, de la clase en vivo 37

---
+++ modulo 8 - JS para frontend/Clase 37 - Manipulando elementos con JavaScript/9 - Hacia la clase en vivo/2 - Clase en vivo/materiales-para-la-clase/public/js/moviesEdit.js
@@ -0,0 +1,7 @@
+const $h1 = document.querySelector("h1");
+const $section = document.querySelector("section");
+const $article = document.querySelector("article");
+
+$h1.classList.add("titulo");
+$article.classList.add("fondoTransparente");
+$section.classList.add("fondoCRUD");
