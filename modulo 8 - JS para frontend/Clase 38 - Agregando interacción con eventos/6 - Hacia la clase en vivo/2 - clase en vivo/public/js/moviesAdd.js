9acf3fd773e32184d12b35517390e2e949a09b2b
GuillermoCochrane <guilleac81@gmail.com>
2024-03-30 23:45:36 -0300

    Finalizado Micro desafío: Paso 2, de la clase en vivo 38

--- modulo 8 - JS para frontend/Clase 38 - Agregando interacción con eventos/6 - Hacia la clase en vivo/2 - clase en vivo/public/js/moviesAdd.js
+++ modulo 8 - JS para frontend/Clase 38 - Agregando interacción con eventos/6 - Hacia la clase en vivo/2 - clase en vivo/public/js/moviesAdd.js
@@ -2,6 +2,8 @@ window.onload = function(){
     let titulo = document.querySelector('.moviesAddTitulo')
     let formulario = document.querySelector('#formulario');
     let article = document.querySelector('article');
+    let $title = document.querySelector("#titulo")
+
     titulo.innerHTML = 'AGREGAR PELÍCULA';
     titulo.classList.add('titulo');
     article.classList.add('fondoTransparente');
@@ -14,5 +16,29 @@ window.onload = function(){
     titulo.addEventListener("mouseover", ()=>{
         const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
         titulo.style.color = rndCol
+    })
+
+    let status = 0
+    //let frase = "" (para buscador)
+
+    console.log($title);
+    $title.addEventListener("keyup",(e)=>{
+        let key = (e.key).toUpperCase();;
+        //frase += key (para buscador)
+        if(key == "S"){
+            status = 1;
+        } else if(key == "E" && status == 1){
+            status = 2;
+        } else if(key == "C" && status == 2){
+            status = 3;
+        } else if(key == "R" && status == 3){
+            status = 4;
+        } else if (key == "E" && status == 4){
+            status = 5;
+        } else if (key == "T" && status == 5){
+            alert("SECRETO MAGICO")
+        } else {
+            status = 0
+        }
     })
 }
