a1c1f55029f277b4cc0d63dabdf5a2bddca4e423
GuillermoCochrane <guilleac81@gmail.com>
2024-04-04 17:44:55 -0300

    Finalizado ejercicio 2, de la clase en vivo 40

--- modulo 8 - JS para frontend/Clase 40 - Pedidos asincrónicos con JavaScript/6 - Hacia la clase en vivo/1 - intro/public/javascripts/scriptE2.js
+++ modulo 8 - JS para frontend/Clase 40 - Pedidos asincrónicos con JavaScript/6 - Hacia la clase en vivo/1 - intro/public/javascripts/scriptE2.js
@@ -1,9 +1,31 @@
 window.addEventListener('load', function() {
-  let body = this.document.querySelector("body");
-  sessionStorage.setItem('bgColor', 'red');
-  sessionStorage.removeItem('font');
-  let a = confirm("¡desea cambiar el color de fondo?");
-  if(a){
-    body.style.background = sessionStorage.getItem("bgColor")
-  }  
+  let greetings = document.querySelector("#greetings");
+  let save = document.querySelector("#save");
+  let del = document.querySelector("#delete");
+
+  let user = localStorage.getItem("user")
+
+  if (user){
+    save.setAttribute("hidden","")
+    del.removeAttribute("hidden","")
+    let userInfo = JSON.parse(user);
+    greetings.innerText = "Hola " + userInfo.user;
+    del.addEventListener("click", () =>{
+      this.localStorage.clear("user");
+      window.location.reload();
+    })
+  } else {
+    greetings.innerText = "Hola visitante";
+    save.removeAttribute("hidden", "");
+    del.setAttribute("hidden", "")
+    save.addEventListener("click", ()=>{
+      let data = prompt("Ingrese su nombre");
+      let userInfo = {}
+      userInfo.user = data;
+      let userData = JSON.stringify(userInfo);
+      localStorage.setItem("user",userData)
+      window.location.reload()
+    })
+  }
+
 });
