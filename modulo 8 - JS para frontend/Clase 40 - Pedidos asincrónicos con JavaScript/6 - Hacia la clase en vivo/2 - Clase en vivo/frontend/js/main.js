d322f1c336fe117effcad402977d017711e8aa0b
GuillermoCochrane <guilleac81@gmail.com>
2024-04-07 05:58:20 -0300

    Finalizados incisos 1 y 2 del Micro desafío: Paso 2: de la clase 40. Agregado estilos para favoritos en la pagina principal

--- modulo 8 - JS para frontend/Clase 40 - Pedidos asincrónicos con JavaScript/6 - Hacia la clase en vivo/2 - Clase en vivo/frontend/js/main.js
+++ modulo 8 - JS para frontend/Clase 40 - Pedidos asincrónicos con JavaScript/6 - Hacia la clase en vivo/2 - Clase en vivo/frontend/js/main.js
@@ -1,22 +1,16 @@
-window.onload = () => {
+window.addEventListener("load", () => {
   const app = document.getElementById("root");
   const container = document.createElement("div");
   container.setAttribute("class", "container");
   app.appendChild(container);
-  let peliculas = [];
-
-  // Aqui debemos agregar nuestro fetch
-  fetch("http://localhost:3031/api/movies")
-  .then(function(response){
-  return response.json();
-  })
-  .then(function(info){
-    peliculas = info;
+  let favoritas = JSON.parse(localStorage.getItem("fav")) || [];
   
-
-  // Codigo que debemos usar para mostrar los datos en el frontend
-    let data = peliculas.data;
-
+  fetch("http://localhost:3031/api/movies")
+  .then(function(r){
+    return r.json();
+  })
+  .then(function(i){
+    let data = i.data;
     data.forEach((movie) => {
       const card = document.createElement("div");
       card.setAttribute("class", "card");
@@ -27,18 +21,45 @@ window.onload = () => {
       const p = document.createElement("p");
       p.textContent = `Rating: ${movie.rating}`;
 
-      const duracion = document.createElement("p");
-      duracion.textContent = `Duración: ${movie.length}`;
+      const dur = document.createElement("p");
+      dur.textContent = `Duración: ${movie.length}`;
+
+      const star = document.createElement("button");
+      star.textContent = "⭐";
+      star.setAttribute("class", "star-button");
+      star.setAttribute("id",movie.id);
+
+      for (const f of favoritas) {
+        f.id == movie.id && f.fav == true ? star.classList.toggle("fav") : null
+      }
+
+      star.addEventListener("click", function () {
+        const bID = star.getAttribute("id");
+        let found = false;
+        for (const m of favoritas) {
+          if (m.id === bID) {
+            m.fav = !m.fav;
+            found = true;
+          }
+        }
+        if (!found) {
+          favoritas.push({ id: bID, fav: true });
+        }
+        star.classList.toggle("fav")
+        localStorage.setItem("fav", JSON.stringify(favoritas));
+        console.log(localStorage);
+      });
 
       container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
       if (movie.genre !== null) {
-        const genero = document.createElement("p");
-        genero.textContent = `Genero: ${movie.genre.name}`;
-        card.appendChild(genero);
+        const gen = document.createElement("p");
+        gen.textContent = `Genero: ${movie.genre.name}`;
+        card.appendChild(gen);
       }
-      card.appendChild(duracion);
+      card.appendChild(dur);
+      card.appendChild(star);
     });
- })
-};
+  })
+});
