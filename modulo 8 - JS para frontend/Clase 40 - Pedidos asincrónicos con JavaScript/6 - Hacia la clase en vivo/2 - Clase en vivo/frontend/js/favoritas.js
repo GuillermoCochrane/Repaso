106bb8d94f5139fb502809e77807255d59d8d492
GuillermoCochrane <guilleac81@gmail.com>
2024-04-07 21:43:35 -0300

    Finalizada página que muestra películas favoritas, y si no hay favoritas, muestra mensaje que aun no hay favoritos, del Micro desafío:Paso 2, de la clase en vivo 0

--- modulo 8 - JS para frontend/Clase 40 - Pedidos asincrónicos con JavaScript/6 - Hacia la clase en vivo/2 - Clase en vivo/frontend/js/favoritas.js
+++ modulo 8 - JS para frontend/Clase 40 - Pedidos asincrónicos con JavaScript/6 - Hacia la clase en vivo/2 - Clase en vivo/frontend/js/favoritas.js
@@ -3,36 +3,46 @@ window.onload = () => {
   const container = document.createElement("div");
   container.setAttribute("class", "container");
   app.appendChild(container);
-
-  // Aqui debemos agregar nuestro fetch
-
-
-
-  /** Codigo que debemos usar para mostrar los datos en el frontend
-    let data = peliculas.data;
-
+  let favoritas = JSON.parse(localStorage.getItem("fav")) || [];
+  let counter = 0;
+  fetch("http://localhost:3031/api/movies")
+  .then(function(r){
+    return r.json();
+  })
+  .then(function(i){
+    let data = i.data;
+    let foundFavorites = false;
     data.forEach((movie) => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = movie.title;
-
-      const p = document.createElement("p");
-      p.textContent = `Rating: ${movie.rating}`;
-
-      const duracion = document.createElement("p");
-      duracion.textContent = `Duración: ${movie.length}`;
-
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-      if (movie.genre !== null) {
-        const genero = document.createElement("p");
-        genero.textContent = `Genero: ${movie.genre.name}`;
-        card.appendChild(genero);
-      }
-      card.appendChild(duracion);
+      for (const fav of favoritas) {
+        if( fav.id == movie.id && fav.fav == true){
+          const card = document.createElement("div");
+          card.setAttribute("class", "card");
+    
+          const h1 = document.createElement("h1");
+          h1.textContent = movie.title;
+    
+          const p = document.createElement("p");
+          p.textContent = `Rating: ${movie.rating}`;
+    
+          const duracion = document.createElement("p");
+          duracion.textContent = `Duración: ${movie.length}`;
+    
+          container.appendChild(card);
+          card.appendChild(h1);
+          card.appendChild(p);
+          if (movie.genre !== null) {
+            const genero = document.createElement("p");
+            genero.textContent = `Genero: ${movie.genre.name}`;
+            card.appendChild(genero);
+          }
+          card.appendChild(duracion);
+          foundFavorites = true;
+        }}
     });
-  */
+    if (!foundFavorites) {
+      const noFavoritesMessage = document.createElement("h2");
+      noFavoritesMessage.textContent = "Aún no tiene favoritos";
+      container.appendChild(noFavoritesMessage);
+    }
+  })
 };
