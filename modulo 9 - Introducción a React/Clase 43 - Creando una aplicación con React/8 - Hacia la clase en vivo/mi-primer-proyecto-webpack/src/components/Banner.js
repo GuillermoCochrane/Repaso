edbf94379aafc12855c3abaeb39fc387b110b853
GuillermoCochrane <guilleac81@gmail.com>
2024-05-22 18:49:17 -0300

    Finalizado Micro desafíos - Paso 4, de hacia la clase en vivo 43, de app de React hecha con webpack

--- modulo 9 - Introducción a React/Clase 43 - Creando una aplicación con React/8 - Hacia la clase en vivo/mi-primer-proyecto-webpack/src/components/Banner.js
+++ modulo 9 - Introducción a React/Clase 43 - Creando una aplicación con React/8 - Hacia la clase en vivo/mi-primer-proyecto-webpack/src/components/Banner.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 function Banner(){
     return (
-        <section class="banner">
-            <h1 class="titulo">Tenemos lo mejor para ti...</h1>
+        <section className="banner">
+            <h1 className="titulo">Tenemos lo mejor para ti...</h1>
         </section>
     )
 
