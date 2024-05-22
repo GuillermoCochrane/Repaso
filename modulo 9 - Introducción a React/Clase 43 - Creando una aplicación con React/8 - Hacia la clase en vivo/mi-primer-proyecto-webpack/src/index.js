e16e5ffeaf910019e544a1f2e41c7ddec9bfa0c6
GuillermoCochrane <guilleac81@gmail.com>
2024-05-22 18:36:40 -0300

    Finalizado Micro desafíos - Paso 2, de hacia la clase en vivo 43, de app de React hecha con webpack

--- modulo 9 - Introducción a React/Clase 43 - Creando una aplicación con React/8 - Hacia la clase en vivo/mi-primer-proyecto-webpack/src/index.js
+++ modulo 9 - Introducción a React/Clase 43 - Creando una aplicación con React/8 - Hacia la clase en vivo/mi-primer-proyecto-webpack/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 //import './index.css';
 //import App from './App';
 import './assets/css/master.css';
-import Web from './Web';
+import Web from './components/Web';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
