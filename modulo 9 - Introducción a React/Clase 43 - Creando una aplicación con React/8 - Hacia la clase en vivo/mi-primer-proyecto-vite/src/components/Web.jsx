45b74b611ee6c09d73476cdc2f13a56c90f4b97a
GuillermoCochrane <guilleac81@gmail.com>
2024-05-22 19:28:50 -0300

    Finalizado Micro desafíos - Paso 3, de hacia la clase en vivo 43, de app de React hecha con Vite

--- modulo 9 - Introducción a React/Clase 43 - Creando una aplicación con React/8 - Hacia la clase en vivo/mi-primer-proyecto-vite/src/components/Web.jsx
+++ modulo 9 - Introducción a React/Clase 43 - Creando una aplicación con React/8 - Hacia la clase en vivo/mi-primer-proyecto-vite/src/components/Web.jsx
@@ -1,8 +1,10 @@
 import Encabezado from './Encabezado';
+import Banner from './Banner';
 function Web(){
   return (
     <div className="container">
       <Encabezado />
+      <Banner />
     </div>
   )
 
