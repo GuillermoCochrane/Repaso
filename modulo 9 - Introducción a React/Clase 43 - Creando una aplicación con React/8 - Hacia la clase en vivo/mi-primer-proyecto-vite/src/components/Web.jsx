873c06068c9a2942b2336fe2be816a3fe2f321c7
GuillermoCochrane <guilleac81@gmail.com>
2024-05-22 19:23:51 -0300

    Finalizado Micro desafíos - Paso 1, de hacia la clase en vivo 43, de app de React hecha con Vite

---
+++ modulo 9 - Introducción a React/Clase 43 - Creando una aplicación con React/8 - Hacia la clase en vivo/mi-primer-proyecto-vite/src/components/Web.jsx
@@ -0,0 +1,11 @@
+
+function Web(){
+  return (
+    <div className="container">
+
+      <h1>Hola Mundo</h1>
+    </div>
+  )
+
+}
+export default Web;
---
+++ modulo 9 - Introducción a React/Clase 43 - Creando una aplicación con React/8 - Hacia la clase en vivo/mi-primer-proyecto-vite/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Web from './components/Web'
+import "./assets/css/master.css"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Web />
+  </React.StrictMode>,
+)
