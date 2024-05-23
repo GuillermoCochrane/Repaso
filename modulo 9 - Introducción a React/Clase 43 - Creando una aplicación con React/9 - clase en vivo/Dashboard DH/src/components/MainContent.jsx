267a685315a4bcac6cbe0fb5834e554541492b74
GuillermoCochrane <guilleac81@gmail.com>
2024-05-23 02:19:50 -0300

    Agregado componente MainContent.jsx

---
+++ modulo 9 - Introducción a React/Clase 43 - Creando una aplicación con React/9 - clase en vivo/Dashboard DH/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import Topbar from "./Topbar"
+import ContentRowTop from "./ContentRowTop"
+function MainContent() {
+    return (
+        <div id="content">
+            <Topbar/>
+            <ContentRowTop/>
+        </div>
+    )
+}
+
+export default MainContent
