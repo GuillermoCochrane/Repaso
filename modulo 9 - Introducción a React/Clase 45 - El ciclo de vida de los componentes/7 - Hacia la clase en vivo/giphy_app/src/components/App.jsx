4ec1bfaf283b1b8900ce07f9b0b2f0b0b335a5c1
GuillermoCochrane <guilleac81@gmail.com>
2024-05-24 19:07:38 -0300

    Micro desafío: Paso 3, Parte 1

--- modulo 9 - Introducción a React/Clase 45 - El ciclo de vida de los componentes/7 - Hacia la clase en vivo/giphy_app/src/components/App.jsx
+++ modulo 9 - Introducción a React/Clase 45 - El ciclo de vida de los componentes/7 - Hacia la clase en vivo/giphy_app/src/components/App.jsx
@@ -1,64 +1,25 @@
 import './app.css'
+import NavBar from './NavBar/NavBar'
+import Gif from './Gif/Gif'
 
 function App() {
   return (
       <>
 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-          <div className="container">
-            <a className="navbar-brand" href="#">GIPHY APP</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="nav ml-auto">
-              <li className="nav-item">
-                <button className="btn btn-success">Cargar random</button>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <NavBar />
 
         <div className="container">
 
           <div className="row text-center">
 
-            <div className="col-lg-3 col-md-6 mb-4">
-              <div className="card h-100">
-                <img className="card-img-top" src="" alt=""/>
-                <div className="card-body">
-                  <h4 className="card-title">Titulo</h4>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mb-4">
-              <div className="card h-100">
-                <img className="card-img-top" src="" alt=""/>
-                <div className="card-body">
-                  <h4 className="card-title">Titulo</h4>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mb-4">
-              <div className="card h-100">
-                <img className="card-img-top" src="" alt=""/>
-                <div className="card-body">
-                  <h4 className="card-title">Titulo</h4>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mb-4">
-              <div className="card h-100">
-                <img className="card-img-top" src="" alt=""/>
-                <div className="card-body">
-                  <h4 className="card-title">Titulo</h4>
-                </div>
-              </div>
-            </div>
+            <Gif />
+            <Gif />
+            <Gif />
+            <Gif />
+            <Gif />
+            <Gif />
+            <Gif />
+            <Gif />
 
           </div>
 
---
+++ modulo 9 - Introducción a React/Clase 45 - El ciclo de vida de los componentes/7 - Hacia la clase en vivo/giphy_app/src/components/Gif/Gif.jsx
@@ -0,0 +1,14 @@
+function Gif() {
+    return (
+        <div className="col-lg-3 col-md-6 mb-4">
+              <div className="card h-100">
+                <img className="card-img-top" src="" alt=""/>
+                <div className="card-body">
+                  <h4 className="card-title">Titulo</h4>
+                </div>
+              </div>
+            </div>
+    )
+}
+
+export default Gif
