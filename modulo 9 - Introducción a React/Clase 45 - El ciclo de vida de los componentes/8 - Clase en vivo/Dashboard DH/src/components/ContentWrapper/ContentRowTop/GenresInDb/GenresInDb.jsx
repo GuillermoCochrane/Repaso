8123b18a45150a68342391b4220d99f8829dfb92
GuillermoCochrane <guilleac81@gmail.com>
2024-05-25 17:53:28 -0300

    Finalizado Micro desafío: Paso 1, de la clase en vivo 45

--- modulo 9 - Introducción a React/Clase 45 - El ciclo de vida de los componentes/8 - Clase en vivo/Dashboard DH/src/components/ContentWrapper/ContentRowTop/GenresInDb/GenresInDb.jsx
+++ modulo 9 - Introducción a React/Clase 45 - El ciclo de vida de los componentes/8 - Clase en vivo/Dashboard DH/src/components/ContentWrapper/ContentRowTop/GenresInDb/GenresInDb.jsx
@@ -1,35 +1,54 @@
+import { Component } from 'react';
 import GenreCard from './GenreCard/GenreCard';
-let genres = [
-    {genreName: 'Acción'},
-    {genreName: 'Animación'},
-    {genreName: 'Aventura'},
-    {genreName: 'CienciaFicción'},
-    {genreName: 'Comedia'},
-    {genreName: 'Documental'},
-    {genreName: 'Drama'},
-    {genreName: 'Fantasia'},
-    {genreName: 'Infantiles'},
-    {genreName: 'Musical'},
-]
 
-function GenresInDb() {
-    return (
-        <div className="col-lg-6 mb-4">						
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
-                </div>
-                <div className="card-body">
-                    <div className="row">
-                        {
-                            genres.map((genre,i) => 
-                                <GenreCard genreName={genre.genreName} key={`item-${genre.genreName}${i}`}/>
-                            )
-                        }
+class GenresInDb extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            genres: []
+        }
+    }
+
+    getGenres = async () => {
+        const response = await fetch('/api/api/genres');
+        const data = await response.json();
+        console.log(data.data)
+        this.setState({
+            genres: data.data
+        });
+    }
+
+    componentDidMount() {
+        try {
+            this.getGenres();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    render() {  
+            return (
+                <div className="col-lg-6 mb-4">						
+                    <div className="card shadow mb-4">
+                        <div className="card-header py-3">
+                            <h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
+                        </div>
+                        <div className="card-body">
+                            <div className="row">
+                                {   
+                                    this.state.genres.length == 0 ?
+                                    <p className="text-center w-100 text-muted">No genres in database</p> :
+                                    this.state.genres.map((genre, i) =>
+                                        <GenreCard genreName={genre.name} key={`item-${genre.name}${i}
+                                    `}/>
+                                    )
+                                }
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-		</div>
-    );
+            );
+    }
 }
 export default GenresInDb;
