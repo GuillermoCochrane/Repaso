60cd08acd47a57aa90c3e711a15ce99c995b893b
GuillermoCochrane <guilleac81@gmail.com>
2024-12-11 02:01:49 -0300

    Agregada ruta de todos los actores,  del Bonus track, de la clase en vivo 31, del modulo 6

---
+++ modulo 6 - Base de Datos/Clase 31 - Introducción, modelos y consultas básicas/9 - Clase en vivo/ejercicio/src/controllers/actorsController.js
@@ -0,0 +1,16 @@
+const db = require("../database/models");
+const { Actores } = db;
+
+let actorsController = {
+  list: function(req, res) {
+    Actores.findAll()
+      .then(actors => {
+        res.render('actorsList', { actors });
+      })
+      .catch(error => {
+        res.status(500).send(error);
+      });
+  },
+};
+
+module.exports = actorsController;
