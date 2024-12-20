3ac0929c34af06aa163b90843e371c6fc1e6cd54
GuillermoCochrane <guilleac81@gmail.com>
2024-12-20 18:58:10 -0300

    Agregada ruta: "/movies/add (GET)"., del Micro desafío: Paso 2, de la clase en vivo 33, del modulo 6

--- modulo 6 - Base de Datos/Clase 33 - Relaciones y CRUD completo/Clase en vivo/ejercicio/src/controllers/moviesController.js
+++ modulo 6 - Base de Datos/Clase 33 - Relaciones y CRUD completo/Clase en vivo/ejercicio/src/controllers/moviesController.js
@@ -20,12 +20,14 @@ const moviesController = {
                 res.render('moviesList.ejs', {movies})
             })
     },
+
     'detail': (req, res) => {
         db.Movie.findByPk(req.params.id)
             .then(movie => {
                 res.render('moviesDetail.ejs', {movie});
             });
     },
+
     'new': (req, res) => {
         db.Movie.findAll({
             order : [
@@ -37,6 +39,7 @@ const moviesController = {
                 res.render('newestMovies', {movies});
             });
     },
+
     'recomended': (req, res) => {
         db.Movie.findAll({
             where: {
@@ -50,10 +53,15 @@ const moviesController = {
                 res.render('recommendedMovies.ejs', {movies});
             });
     },
+
     //Aqui dispongo las rutas para trabajar con el CRUD
     add: function (req, res) {
-        
+        Genres.findAll()
+            .then(genres => {
+                res.render('moviesAdd.ejs', {allGenres: genres});
+            });
     },
+
     create: function (req,res) {
 
     },
